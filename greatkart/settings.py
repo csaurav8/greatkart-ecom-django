1534d29c3e77c2fc9211c45a204c16188b08fc61
csaurav8 <official.csaurav8@gmail.com>
2024-02-06 05:56:33 +0530

    Added functionality to display products by category and Created a new context processor menu-links

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'category',
     'accounts',
+    'store',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -64,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'category.context_processors.menu_links'
             ],
         },
     },
---
+++ store/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Product
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('product_name', 'price', 'stock', 'category', 'modified_at')
+    prepopulated_fields = {'slug':('product_name',)}
+
+# Register your models here.
+admin.site.register(Product, ProductAdmin)
